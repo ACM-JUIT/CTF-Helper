b34ab5ade89cb13c5e934f0dd41584ae018ae875
haripriya75 <haripriyaradhika28@gmail.com>
2021-07-11 20:40:06 +0530

    Tools

--- CTF-Helper(windows)/install_tools/binary/apktool.py
+++ CTF-Helper(windows)/install_tools/binary/apktool.py
@@ -5,7 +5,7 @@ import requests
 subprocess.call("pip install requests", shell=True)
 url = 'https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.5.0.jar'
 r = requests.get(url, allow_redirects=True)
-os.chdir("../../Tools/apktool")
+os.chdir("../../Tools")
 open('apktool.jar', 'wb').write(r.content)
 # walk_dir = sys.argv[1]
 # i = 0
